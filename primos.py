7b33a92d4b3d4f30e6178022d79278679bd44404
FelipeIgansi <felipeigansi2009@hotmail.com>
2020-10-04 21:32:41 -0300

    Primeiro_commit

---
+++ primos.py
@@ -0,0 +1,22 @@
+qtnDivisores = 0
+
+listaPrimos = []
+
+arquivo = open("Primos.txt", "w")
+
+arquivo.write("Esses são os primos encontrados e gerados pelo python:\n\n")
+
+for numero in range(1,1000):
+    for divisor in range(1,((numero)//2)+1):
+        if ((numero%divisor) ==0):
+            qtnDivisores += 1
+            
+        
+    if (qtnDivisores == 1):
+        #print(i)
+        listaPrimos.append(numero)
+        arquivo.write(str(numero)+"\n")
+    qtnDivisores = 0
+
+print(listaPrimos)
+
