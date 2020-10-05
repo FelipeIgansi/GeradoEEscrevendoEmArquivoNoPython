a47a1bb55c3a59ea8b9ff77370f2c0d44b43b324
FelipeIgansi <lipeigansi@gmail.com>
2020-10-05 19:33:30 -0300

    GeracaoPassouASerEmCSV

--- primos.py
+++ primos.py
@@ -4,12 +4,12 @@ listaPrimos = []
 
 contador = 0
 
-arquivoTxt = open("/media/felipe/HD500/Code/python/Primos.txt", "w")
-arquivoExcel = open("/media/felipe/HD500/Code/python/Primos.xlsx", "w")
+arquivoCSV = open("/media/felipe/HD500/Code/python/Primos.csv", "w")
+#arquivoExcel = open("/media/felipe/HD500/Code/python/Primos.xlsx", "w")
 
-arquivoTxt.write("Esses são os primos encontrados e gerados pelo python:\n\n")
+arquivoCSV.write("Esses são os primos encontrados e gerados pelo python:\n\n")
 
-for numero in range(1,100):
+for numero in range(1,100000):
     for divisor in range(1,((numero)//2)+1):
         if ((numero%divisor) ==0):
             qtnDivisores += 1
@@ -18,14 +18,14 @@ for numero in range(1,100):
     if (qtnDivisores == 1):
         #print(numero)
         listaPrimos.append(numero)
-        arquivoTxt.write(str(numero)+"\n")
-        arquivoExcel.write(str(numero)+"\n")
-        contador += 1
-    qtnDivisores = 0
+        arquivoCSV.write(str(numero)+",\n")
+        #arquivoExcel.write(str(numero)+",\n")
+        contador += 1 # Faz autoincremento da quantidade de valores gerados
+    qtnDivisores = 0 # Zera a quantidade de divisores
 
 print(listaPrimos)
 
-arquivoTxt.write("\n\nForam gerados {} numeros primos.".format(str(contador)))
+arquivoCSV.write("\n\nForam gerados {} numeros primos.".format(str(contador)))
 
-arquivoTxt.close()
-arquivoExcel.close()
+arquivoCSV.close()
+#arquivoExcel.close()
